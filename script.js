c16f66926224a1b26a2f56246d2a833b0a170788
ac8479 <ac8479@nyu.edu>
2021-05-06 06:02:14 -0700

    finished content sections, improved scroll, added menu

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 var rellax = new Rellax('.rellax');
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+AOS.init();
+new Glide('.glide').mount()
